12681fdd90ca97f9597ba9e03f24cb8c046732c5
Régis GEROMEGNACE <141682@supinfo.com>
2013-04-22 21:42:34 -0400

    Implémentation de la prise d'une photographie

--- CarWreckLocator/src/com/supinfogp/carwrecklocator/CarWreckLocator.java
+++ CarWreckLocator/src/com/supinfogp/carwrecklocator/CarWreckLocator.java
@@ -1,8 +1,13 @@
 package com.supinfogp.carwrecklocator;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.supinfogp.camera.CameraActivity;
 
 public class CarWreckLocator extends Activity {
 
@@ -10,6 +15,17 @@ public class CarWreckLocator extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_wreck_locator);
+        
+        final Button btnTakePhoto = (Button)this.findViewById(R.id.btn_take_photo);
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(getApplicationContext(), CameraActivity.class);
+				startActivity(i);
+			}
+		});
     }
 
     @Override
