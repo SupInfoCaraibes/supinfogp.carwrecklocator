d617491399c1423f62cea7f375458fc729cc4b81
Régis GEROMEGNACE <141682@supinfo.com>
2013-04-28 23:15:04 -0400

    Implémentation saisie information épave

--- CarWreckLocator/gen/com/supinfogp/carwrecklocator/R.java
+++ CarWreckLocator/gen/com/supinfogp/carwrecklocator/R.java
@@ -8,30 +8,51 @@
 package com.supinfogp.carwrecklocator;
 
 public final class R {
+    public static final class array {
+        public static final int car_brands=0x7f040000;
+        public static final int car_colors=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int btn_capture=0x7f070002;
-        public static final int btn_take_photo=0x7f070000;
-        public static final int camera_preview=0x7f070001;
-        public static final int menu_settings=0x7f070003;
+        public static final int btnDataFormCancel=0x7f08000c;
+        public static final int btnDataFormOk=0x7f08000d;
+        public static final int btn_capture=0x7f080002;
+        public static final int btn_take_photo=0x7f080000;
+        public static final int camera_preview=0x7f080001;
+        public static final int editTextCarRegistration=0x7f08000b;
+        public static final int menu_settings=0x7f08000e;
+        public static final int spinnerCarBrand=0x7f080005;
+        public static final int spinnerCarColor=0x7f080008;
+        public static final int tableRow1=0x7f080003;
+        public static final int tableRow2=0x7f080006;
+        public static final int tableRow3=0x7f080009;
+        public static final int txtCarBrand=0x7f080004;
+        public static final int txtCarColor=0x7f080007;
+        public static final int txtCarRegistration=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_car_wreck_locator=0x7f030000;
         public static final int camera_activity_view=0x7f030001;
+        public static final int carwreck_dataform_view=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_car_wreck_locator=0x7f060000;
+        public static final int activity_car_wreck_locator=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int btn_capture_photo=0x7f040004;
-        public static final int btn_take_photo=0x7f040003;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int btn_capture_photo=0x7f050004;
+        public static final int btn_dataform_cancel=0x7f050005;
+        public static final int btn_dataform_validate=0x7f050006;
+        public static final int btn_take_photo=0x7f050003;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int txt_dataform_car_brand=0x7f050007;
+        public static final int txt_dataform_car_color=0x7f050008;
+        public static final int txt_dataform_carregistration=0x7f050009;
     }
     public static final class style {
         /** 
@@ -55,10 +76,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
