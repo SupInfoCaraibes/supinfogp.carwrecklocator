0e6fd018c80a7112d30a199503e03dd73e98637a
Régis GEROMEGNACE <141682@supinfo.com>
2013-05-11 23:12:53 -0400

    implémentation de l'activité de saisie des informations sur l'épave

--- CarWreckLocator/src/com/supinfogp/carwrecklocator/CarWreckDataForm.java
+++ CarWreckLocator/src/com/supinfogp/carwrecklocator/CarWreckDataForm.java
@@ -4,23 +4,32 @@
 package com.supinfogp.carwrecklocator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * @author rgeromegnace
  *
  */
 public class CarWreckDataForm extends Activity {
-
+	
+	private Spinner spinnerCarBrand = null;
+	private Spinner spinnerCarColor = null;
+	private TextView editTextCarRegistration = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.carwreck_dataform_view);
 		
-		Spinner spinnerCarBrand = (Spinner) findViewById(R.id.spinnerCarBrand);
+		spinnerCarBrand = (Spinner) findViewById(R.id.spinnerCarBrand);
 		// Create an ArrayAdapter using the string array and a default spinner layout
 		ArrayAdapter<CharSequence> carBrandAdapter = ArrayAdapter.createFromResource(this,
 		        R.array.car_brands, android.R.layout.simple_spinner_item);
@@ -29,7 +38,7 @@ public class CarWreckDataForm extends Activity {
 		// Apply the adapter to the spinner
 		spinnerCarBrand.setAdapter(carBrandAdapter);
 
-		Spinner spinnerCarColor = (Spinner) findViewById(R.id.spinnerCarBrand);
+		spinnerCarColor = (Spinner) findViewById(R.id.spinnerCarColor);
 		// Create an ArrayAdapter using the string array and a default spinner layout
 		ArrayAdapter<CharSequence> carColorAdapter = ArrayAdapter.createFromResource(this,
 		        R.array.car_colors, android.R.layout.simple_spinner_item);
@@ -37,6 +46,43 @@ public class CarWreckDataForm extends Activity {
 		carColorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		// Apply the adapter to the spinner
 		spinnerCarColor.setAdapter(carColorAdapter);
+		
+		editTextCarRegistration = (TextView) findViewById(R.id.editTextCarRegistration);
+		
+		Button btnDataFormOk = (Button)findViewById(R.id.btnDataFormOk);
+		btnDataFormOk.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				String carBrand = (String) spinnerCarBrand.getSelectedItem();
+				Log.i("CWL", "marque: "+carBrand);
+				
+				String carColor = (String) spinnerCarColor.getSelectedItem();
+				Log.i("CWL", "couleur: "+carColor);
+				
+				//j'utilise final car le contenu de getText est non modifiable
+				final String carRegistration = editTextCarRegistration.getText().toString();
+				Log.i("CWL", "immatriculation: "+carRegistration);
+				
+				Intent result = new Intent();
+				String [] data = {carBrand, carColor,carRegistration};
+				result.putExtra("com.supinfogp.carwrecklocator.photodata", data);
+				setResult(Activity.RESULT_OK, result);
+				finish();
+			}
+		});
+		
+		Button btnDataFormCancel = (Button)findViewById(R.id.btnDataFormCancel);
+		btnDataFormCancel.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				setResult(Activity.RESULT_CANCELED);
+				finish();
+			}
+		});
 	}
 
 }
--- CarWreckLocator/src/com/supinfogp/carwrecklocator/CarWreckLocator.java
+++ CarWreckLocator/src/com/supinfogp/carwrecklocator/CarWreckLocator.java
@@ -3,6 +3,7 @@ package com.supinfogp.carwrecklocator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -10,8 +11,26 @@ import android.widget.Button;
 import com.supinfogp.camera.CameraActivity;
 
 public class CarWreckLocator extends Activity {
-
+	
+	static final int CAR_WECK_DATA_FORM_REQUEST = 0;
+	
     @Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		if (requestCode == CAR_WECK_DATA_FORM_REQUEST) {
+			if (resultCode == RESULT_OK) {
+				String [] photoData = data.getStringArrayExtra("com.supinfogp.carwrecklocator.photodata");
+				Log.i("CWL", "marque retournée: "+photoData[0]);
+				Log.i("CWL", "couleur retournée: "+photoData[1]);
+				Log.i("CWL", "immatriculation retournée: "+photoData[2]);
+				Intent intentCamera = new Intent(getApplicationContext(), CameraActivity.class);
+				startActivity(intentCamera);
+			}
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_wreck_locator);
@@ -22,8 +41,11 @@ public class CarWreckLocator extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent i = new Intent(getApplicationContext(), CameraActivity.class);
-				startActivity(i);
+				Intent intentDataForm = new Intent(getApplicationContext(), CarWreckDataForm.class);
+				startActivityForResult(intentDataForm, CAR_WECK_DATA_FORM_REQUEST);
+				
+				//Intent intentCamera = new Intent(getApplicationContext(), CameraActivity.class);
+				//startActivity(intentCamera);
 			}
 		});
     }
